b5247388255a76d9b0fcd80cff2a9a2d8e569293
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2024-04-10 13:27:00 +0100

    all task

--- src/components/noreviews.js
+++ src/components/noreviews.js
@@ -14,8 +14,8 @@ const Noreview = () => {
             {showReviewForm ? (
                 <CreateReview />
             ) : (
-                <div className='flex flex-col w-full mx-auto items-center justify-center h-screen'>
-                    <div><img className="items-center mt-5" style={{ width: "250px", height: "150px" }} src={emptyreview} alt="" /></div>
+                <div className='flex flex-col w-full mx-auto items-center justify-center'>
+                    <div><img className="items-center mt-10" style={{ width: "250px", height: "150px" }} src={emptyreview} alt="" /></div>
                     <div className='mt-4'>Oops! No reviews yet.</div>
                     <button onClick={handleClick} className='bg-customBlue text-white px-4 py-2 mt-4 rounded-md'>LEAVE A REVIEW</button>
                 </div>
