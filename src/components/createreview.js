c4d75cff776704c6d0ca3adbbfa7bd76da6fd67b
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2024-04-10 22:02:59 +0100

    all task

--- src/components/createreview.js
+++ src/components/createreview.js
@@ -16,7 +16,7 @@ const CreateReview = () => {
     };
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50">
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-95">
             <div className="bg-white rounded-lg p-8  lg:w-[500px] lg:h-[587px]">
                 <Formik>
                     <form className="flex flex-col gap-6">
